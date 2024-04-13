44f34d39230ae74fbc3d7ebebcc2f3fdb33dd69f
Alejandra Guevara <a.guevarah@uniandes.edu.co>
2024-04-13 16:47:05 -0500

    model testing

--- scripts/05. Pruebas Aleja.R
+++ scripts/05. Pruebas Aleja.R
@@ -770,9 +770,190 @@ predictSample<- predictSample %>%
 write.csv(predictSample,"income_prediction_ln_Ingtotugarr_xgboosting_ale4.csv", row.names = FALSE)
 
 
+#- 6 | Modelo 5: XGBoost predicción de pobreza con variables e interacciones --------------------------
+
+### Arreglo de data ###
+
+#Seleccionamos las variables que necesitamos para predecir el ingreso
+train_hogares1 <- dplyr::select(train_hogares, Pobre, Dominio, Depto, P5010, 
+                                N_cuartos_hog, Nper, nmenores_5, nmenores_6_11, 
+                                nmenores_12_17, nocupados, nincapacitados, ntrabajo_menores, 
+                                Head_Mujer, Head_Afiliado_SS, P5140, Npersug, Lp, Li,
+                                Head_exper_ult_trab, Head_Rec_alimento, Head_Rec_subsidio, 
+                                Head_Rec_vivienda, Head_Ocupacion, maxEducLevel, Head_Primas,
+                                Head_Segundo_trabajo, DormitorXpersona, Ln_Cuota, Head_Oficio,
+                                Ln_Pago_arrien, nmujeres, Ocup_vivienda, 
+                                Head_Cot_pension,Cabecera)
+
+train_hogares1 <- train_hogares1 %>% 
+  dplyr::mutate(
+    Head_Mujer = factor(Head_Mujer),
+    Head_Afiliado_SS = factor(Head_Afiliado_SS),
+    Head_exper_ult_trab = factor(Head_exper_ult_trab),
+    Head_Rec_alimento = factor(Head_Rec_alimento),
+    Head_Rec_subsidio = factor(Head_Rec_subsidio),
+    Head_Rec_vivienda = factor(Head_Rec_vivienda),
+    Head_Ocupacion = factor(Head_Ocupacion),
+    Head_Primas = factor(Head_Primas),
+    Head_Segundo_trabajo = factor(Head_Segundo_trabajo),
+    Head_Oficio = factor(Head_Oficio),
+    Ocup_vivienda = factor(Ocup_vivienda),
+    Head_Cot_pension = factor(Head_Cot_pension),
+    Head_Segundo_trabajo = factor(Head_Segundo_trabajo),
+    Cabecera = factor(Cabecera))
 
 
+#Seleccionamos las mismas variables en la data de test
+test_hogares1 <- test_hogares %>% 
+  dplyr::select(id,Dominio, Depto, P5010, 
+                N_cuartos_hog, Nper, nmenores_5, nmenores_6_11, 
+                nmenores_12_17, nocupados, nincapacitados, ntrabajo_menores, 
+                Head_Mujer, Head_Afiliado_SS, P5140, Npersug, Lp, Li,
+                Head_exper_ult_trab, Head_Rec_alimento, Head_Rec_subsidio, 
+                Head_Rec_vivienda, Head_Ocupacion, maxEducLevel, Head_Primas,
+                Head_Segundo_trabajo, DormitorXpersona, Ln_Cuota, Head_Oficio,
+                Ln_Pago_arrien, nmujeres, Ocup_vivienda, 
+                Head_Cot_pension,Cabecera) %>% 
+  mutate(
+    Head_Mujer = factor(Head_Mujer),
+    Head_Afiliado_SS = factor(Head_Afiliado_SS),
+    Head_exper_ult_trab = factor(Head_exper_ult_trab),
+    Head_Rec_alimento = factor(Head_Rec_alimento),
+    Head_Rec_subsidio = factor(Head_Rec_subsidio),
+    Head_Rec_vivienda = factor(Head_Rec_vivienda),
+    Head_Ocupacion = factor(Head_Ocupacion),
+    Head_Primas = factor(Head_Primas),
+    Head_Segundo_trabajo = factor(Head_Segundo_trabajo),
+    Head_Oficio = factor(Head_Oficio),
+    Ocup_vivienda = factor(Ocup_vivienda),
+    Head_Cot_pension = factor(Head_Cot_pension),
+    Head_Segundo_trabajo = factor(Head_Segundo_trabajo),
+    Cabecera = factor(Cabecera))
 
 
+#Convirtamos las variables categóricas en numéricas
 
+sapply(train_hogares1,class)
+
+train_hogares1 <- train_hogares1 %>% mutate(Dominio = as.numeric(Dominio),
+                                            Depto = as.numeric(Depto),
+                                            Head_Mujer = as.numeric(Head_Mujer),
+                                            Head_Afiliado_SS = as.numeric(Head_Afiliado_SS),
+                                            Head_exper_ult_trab = as.numeric(Head_exper_ult_trab),
+                                            Head_Rec_alimento = as.numeric(Head_Rec_alimento),
+                                            Head_Rec_subsidio = as.numeric(Head_Rec_subsidio),
+                                            Head_Rec_vivienda = as.numeric(Head_Rec_vivienda),
+                                            Head_Ocupacion = as.numeric(Head_Ocupacion),
+                                            maxEducLevel = as.numeric(maxEducLevel),
+                                            Head_Primas = as.numeric(Head_Primas),
+                                            Head_Segundo_trabajo = as.numeric(Head_Segundo_trabajo),
+                                            Head_Oficio = as.numeric(Head_Oficio),
+                                            Ocup_vivienda = as.numeric(Ocup_vivienda),
+                                            Head_Cot_pension = as.numeric(Head_Cot_pension),
+                                            Cabecera = as.numeric(Cabecera))
+
+test_hogares1 <- test_hogares1 %>% mutate(Dominio = as.numeric(Dominio),
+                                          Depto = as.numeric(Depto),
+                                          Head_Mujer = as.numeric(Head_Mujer),
+                                          Head_Afiliado_SS = as.numeric(Head_Afiliado_SS),
+                                          Head_exper_ult_trab = as.numeric(Head_exper_ult_trab),
+                                          Head_Rec_alimento = as.numeric(Head_Rec_alimento),
+                                          Head_Rec_subsidio = as.numeric(Head_Rec_subsidio),
+                                          Head_Rec_vivienda = as.numeric(Head_Rec_vivienda),
+                                          Head_Ocupacion = as.numeric(Head_Ocupacion),
+                                          maxEducLevel = as.numeric(maxEducLevel),
+                                          Head_Primas = as.numeric(Head_Primas),
+                                          Head_Segundo_trabajo = as.numeric(Head_Segundo_trabajo),
+                                          Head_Oficio = as.numeric(Head_Oficio),
+                                          Ocup_vivienda = as.numeric(Ocup_vivienda),
+                                          Head_Cot_pension = as.numeric(Head_Cot_pension),
+                                          Cabecera = as.numeric(Cabecera))
+
+#Revisemos rápidamente
+sapply(train_hogares1,class)
+sapply(train_hogares1,class)
+
+
+# Dividimos la muestra para entrenar al modelo
+set.seed(91519) # Importante definir la semilla. 
+
+inTrain <- createDataPartition(
+  y = train_hogares1$Pobre,## La variable dependiente u objetivo 
+  p = .8, ## Usamos 70%  de los datos en el conjunto de entrenamiento 
+  list = FALSE)
+
+
+train <- train_hogares1[ inTrain,]
+test  <- train_hogares1[-inTrain,]
+
+#Ajuste del modelo
+
+fiveStats <- function(...) c(twoClassSummary(...), defaultSummary(...))  ## Para usar ROC) (u otras más) para tuning
+
+ctrl<- trainControl(method = "cv",
+                    number = 5,
+                    summaryFunction = fiveStats,
+                    classProbs = TRUE, 
+                    verbose=FALSE,
+                    savePredictions = T)
+
+#Cargamos los parámetros del boosting
+grid_xbgoost <- expand.grid(nrounds = c(250,500),
+                            max_depth = c(1, 2),
+                            eta = c(0.1,  0.01), 
+                            gamma = c(0, 1), 
+                            min_child_weight = c(10, 25),
+                            colsample_bytree = c(0.4, 0.7), 
+                            subsample = c(0.7))
+grid_xbgoost
+
+
+#Entrenamos el modelo
+set.seed(91519) # Importante definir la semilla antes entrenar
+Xgboost_tree <- train(Pobre~Dominio + Depto + P5010 + P5010^2 + 
+                        N_cuartos_hog + Nper + nmenores_5 + nmenores_6_11 + 
+                        nmenores_12_17 + nocupados + nincapacitados + ntrabajo_menores + 
+                        Head_Mujer + Head_Afiliado_SS + Npersug + Npersug^2 +
+                        Head_Rec_subsidio + Head_Rec_vivienda + maxEducLevel + 
+                        Head_Segundo_trabajo + DormitorXpersona^2 + Ln_Cuota + Head_Oficio +
+                        Ln_Pago_arrien + nmujeres^2 + Ocup_vivienda + 
+                        Head_Cot_pension + Cabecera,
+                      data = train, 
+                      method = "xgbTree", 
+                      trControl = ctrl,
+                      tuneGrid=grid_xbgoost,
+                      metric = "ROC",
+                      verbosity = 0
+)         
+
+
+Xgboost_tree
+
+
+test<- test  %>% mutate(Pobre_hat=predict(Xgboost_tree,newdata = test,
+                                          type = "raw"))
+
+
+confusionMatrix(data = test$Pobre_hat, 
+                reference = test$Pobre, positive="Yes", mode = "prec_recall")
+
+#F1 = 1.0
+
+#Representemos gráficamente los árboles entrenados
+tree_plot <- xgb.plot.tree(model = Xgboost_tree$finalModel,
+                           trees = 1:2, plot_width = 1000, plot_height = 500)
+tree_plot
+
+## Ahora hagamos la predicción en la data de test hogares
+
+predictSample <- test_hogares1   %>% 
+  mutate(Pobre = predict(Xgboost_tree, newdata = test_hogares1, type = "raw")) %>% dplyr::select(id,Pobre)
+
+
+predictSample<- predictSample %>% 
+  mutate(pobre=ifelse(Pobre=="Yes",1,0)) %>% 
+  dplyr::select(id,pobre)
+
+#Kaggle puntaje = 
+write.csv(predictSample,"classification_xgboosting_ale5.csv", row.names = FALSE)
 
